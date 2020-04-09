3d046c9d10602d6482c6d4a1c45093f1f2dab867
Cédric B <cedricbardaine@gmail.com>
2020-04-09 11:48:46 +0200

    add controlers skelet

---
+++ src/main/java/com/tactfactory/monprojetsb/monprojetsb/controlers/UserControler.java
@@ -0,0 +1,35 @@
+package com.tactfactory.monprojetsb.monprojetsb.controlers;
+
+public class UserControler implements genericControler {
+
+	@Override
+	public void index() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createGet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createPost() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void details() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/tactfactory/monprojetsb/monprojetsb/controlers/genericControler.java
@@ -0,0 +1,28 @@
+package com.tactfactory.monprojetsb.monprojetsb.controlers;
+
+
+public interface genericControler {
+	/**
+	 * Liste l’ensemble des attributs de l’entité pour tout les enregistrements. 
+	 * Permet de naviguer vers la création d’un nouvel élément. 
+	 * Permet de naviguer vers le détails d’un élément. 
+	 * Permet de directement supprimer un élément.
+	 */
+	public void index() ; 
+	/**
+	 * Permet d’afficher la vue de création d’un élément.
+	 */
+	public void createGet() ; 
+	/**
+	 * Permet d’envoyer les informations de l’élément vers le serveur et le sauvegarde dans la base de données.
+	 */
+	public void createPost() ; 
+	/**
+	 * Permet de supprimer un élément de la base de données par son id.
+	 */
+	public void delete() ; 
+	/**
+	 *  Permet d’afficher le détails d’un élément (avec les objets non primitif liés).
+	 */
+	public void details() ; 
+}
