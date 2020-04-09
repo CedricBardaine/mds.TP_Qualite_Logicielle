c44910d7687be0c6f9e5e89fa31cb48af26fe4dd
Cédric B <cedricbardaine@gmail.com>
2020-04-09 14:52:15 +0200

    make javabean out of entities

--- src/main/java/com/tactfactory/monprojetsb/entities/Product.java
+++ src/main/java/com/tactfactory/monprojetsb/entities/Product.java
@@ -1,15 +1,48 @@
 package com.tactfactory.monprojetsb.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Product {
+//pas besoin de l'annotation @Table ? car ca creer bien les tables dans la bdd
+public class Product implements Serializable {
+	
 	@Id 
 	@GeneratedValue
-	Long id ; 
+	private Long id ; 
 	
-	String name ; 
-	float price ; 
+	private String name ; 
+	
+	private float price ;
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	} 
+	
+	
 }
--- src/main/java/com/tactfactory/monprojetsb/entities/User.java
+++ src/main/java/com/tactfactory/monprojetsb/entities/User.java
@@ -1,5 +1,6 @@
 package com.tactfactory.monprojetsb.entities;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -8,13 +9,54 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
-public class User {
+// pas besoin de l'annotation @Table ? car ca creer bien les tables dans la bdd
+public class User implements Serializable {
+	
 	@Id 
 	@GeneratedValue
-	Long id ; 
+	private Long id ; 
 	
-	String firstname ; 
-	String lastname ; 
+	private String firstname ;
+	
+	private String lastname ; 
+	
 	@OneToMany
-	List<Product> products ; 
+	private List<Product> products ;
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	} 
+	
+	
+	
 }
